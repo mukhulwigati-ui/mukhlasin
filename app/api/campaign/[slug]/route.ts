6199d6e1345c32751b525fcbb7f5233be890dbb8
naufalhawari001-stack <avirtualzonecom@gmail.com>
2026-08-26 15:11:47 +0700

    rute

--- app/api/campaign/[slug]/route.ts
+++ app/api/campaign/[slug]/route.ts
@@ -5,8 +5,8 @@ import { createClient } from '@sanity/client';
 export const dynamic = 'force-dynamic';
 
 const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_SANITY_DATASET || 'production',
   useCdn: false, // Wajib false agar data langsung ditarik real-time
   apiVersion: '2024-01-01',
   token: process.env.SANITY_API_TOKEN,
